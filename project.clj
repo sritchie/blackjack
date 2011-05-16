6a16051dda1ce39dd6bddeeaefbe403e8def5988
Sam Ritchie <sritchie09@gmail.com>
2011-05-15 22:23:49 -0700

    adjusted payout system to give back original chips.

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
-                     [clojure-source "1.2.0"]])
+                     [clojure-source "1.2.0"]]
+  :aot [backtype.blackjack.core])
--- src/backtype/blackjack/core.clj
+++ src/backtype/blackjack/core.clj
@@ -1,6 +1,7 @@
 (ns backtype.blackjack.core
   (:refer-clojure :exclude [shuffle])
-  (:use [clojure.java.shell :only (sh)]))
+  (:use [clojure.java.shell :only (sh)])
+  (:gen-class))
 
 ;; ## Blackjack Data Structures
 ;;
@@ -51,13 +52,15 @@
 
 (defn new-game
   "Initializes a new game of Blackjack."
-  [chips decks]
+  [chips decks soft]
   {:deck (new-deck decks)
    :discard []
+   :player empty-hand
+   :dealer empty-hand
    :chips chips
    :current-bet 0
-   :player empty-hand
-   :dealer empty-hand})
+   :turns 0
+   :soft-17? (boolean soft)})
 
 ;; ## Game Play Mechanics
 
@@ -91,7 +94,9 @@
 
 (defn play-hit
   [game hand-kwd]
-  (deal-cards game 1 hand-kwd :show? true))
+  (-> game
+      (deal-cards 1 hand-kwd :show? true)
+      (assoc :turns (-> game :turns inc))))
 
 (defn dump-hands
   "Dumps the contents of the hand into the given discard pile, and
@@ -100,7 +105,8 @@
   (assoc game
     :discard (reduce into (map game [:discard :dealer :player]))
     :dealer empty-hand
-    :player empty-hand))
+    :player empty-hand
+    :turns 0))
 
 ;; ### Hand Scoring
 
@@ -126,14 +132,18 @@
              score-hand)
        hands))
 
-(defn busted?
-  [hand]
-  (not (some #(<= % 21) (score-hand hand))))
+(defn some-hand [hand pred]
+  (some pred (score-hand hand)))
+
+(defn every-hand [hand pred]
+  (every? pred (score-hand hand)))
+
+(defn twenty-one? [hand] (some-hand hand #{21}))
+(defn busted? [hand] (every-hand hand #(> % 21)))
 
 (defn push?
   [hand1 hand2]
-  (let [[s1 s2] (highest-scores hand1 hand2)]
-    (= s1 s2)))
+  (apply = (highest-scores hand1 hand2)))
 
 (defn beats?
   [hand1 hand2]
@@ -141,22 +151,13 @@
     (and (not (busted? hand1))
          (> s1 s2))))
 
-(defn over-16?
-  [hand]
-  (every? #(>= % 17) (score-hand hand)))
-
-(defn twenty-one?
-  "Determines whether or not the given hand is a blackjack."
-  [hand]
-  (some #{21} (score-hand hand)))
-
-
 (defn outcome
-  [game blackjack?]
-  (let [{:keys [dealer player]} game]
+  [game]
+  (let [{:keys [dealer player turns]} game]
     (cond (busted? dealer) :win
           (push? dealer player) :push
-          (beats? player dealer) (if blackjack?
+          (beats? player dealer) (if (and (zero? turns)
+                                          (twenty-one? player))
                                    :blackjack
                                    :win)
           :else :lose)))
@@ -229,22 +230,16 @@
           (recur game)))))
 
 (defn resolve-bet
-  [game result]
+  [game result double?]
   (let [{:keys [chips current-bet]} game
         pay (-> (case result
-                      :blackjack (/ 3 2)
-                      :win 1
+                      :blackjack (/ 5 2)
+                      :win (if double? 3 2)
                       (:lose :push) 0)
                 (* current-bet) Math/floor int)]
     (assoc game
       :chips (+ chips pay)
       :current-bet 0)))
-
-(defn get-move
-  "TODO: Filter the <= bullshit out into a function that takes a
-  binary predicate and the second argument to that predicate."
-  []
-  (prompt "What is your move? Your choices are hit, stay, or exit."))
 
 (defn initial-deal
   [game]
@@ -258,11 +253,11 @@
   (-> game print-interface make-bet initial-deal))
 
 (defn end-turn
-  [game & {:keys [blackjack?]}]
-  (let [result (outcome game blackjack?)
+  [game & {:keys [double?]}]
+  (let [result (outcome game)
         ret-game (-> game
                      (show-hand :dealer)
-                     (resolve-bet result) ;; FIX!!!
+                     (resolve-bet result double?) ;; FIX!!!
                      print-interface
                      (report-outcome result)
                      dump-hands)]
@@ -270,36 +265,57 @@
     ret-game))
 
 (defn dealer-turn
-  [game]
+  "Dealer takes his turn, following the rules of soft 17."
+  [game & {:keys [double?]}]
   (loop [game (show-hand game :dealer)]
-    (let [dealer (:dealer game)]
+    (let [{:keys [dealer player soft-17?]} game
+          hand-test (if soft-17? every-hand some-hand)]
       (print-interface game)
       (Thread/sleep 600)
-      (if (over-16? dealer)
-        (end-turn game)
+      (if (or (hand-test dealer #(>= % 17))
+              (some-hand player #(>= % 21)))
+        (end-turn game :double? double?)
         (recur (play-hit game :dealer))))))
+
+(defn double-down
+  [game]
+  (-> game
+      (play-hit :player)
+      (dealer-turn :double? true)))
+
+(defn try-again []
+  (println "Hmm, sorry, I didn't get that. Let's try again.")
+  (Thread/sleep 1000))
+
+(defn get-move
+  [allow-double?]
+  (prompt (format "What is your move? Your choices are hit, stay, %sand exit."
+                  (if allow-double? "double down, " ""))))
 
 (defn player-turn
   [game]
   (let [game (start-turn game)]
-    (if (twenty-one? (:player game))
-      (end-turn game :blackjack? true)
+    (if (-> game :player twenty-one?)
+      (end-turn game)
       (loop [game game]
-        (print-interface game)    
-        (case (get-move)
-              "exit" :quit
-              "stay" (dealer-turn game)
-              "hit" (let [game (play-hit game :player)
-                          player (:player game)]
-                      (cond (busted? player) (end-turn game)
-                            (twenty-one? player) (dealer-turn game)
-                            :else (recur game)))
-              (do (println "Hmm, sorry, I didn't get that. Let's try again.")
-                  (Thread/sleep 1000)
-                  (recur game)))))))
+        (let [allow-double? (zero? (:turns game))]
+          (print-interface game)
+          (case (get-move allow-double?)
+                "exit" :quit
+                "stay" (dealer-turn game)
+                "hit" (let [game (play-hit game :player)
+                            player (:player game)]
+                        (if (or (busted? player)
+                                (twenty-one? player))
+                          (dealer-turn game)
+                          (recur game)))
+                "double down" (if allow-double?
+                                (double-down game)
+                                (do (try-again) (recur game)))
+                (do (try-again) (recur game))))))))
 
-(defn start []
-  (loop [game (new-game 500 *total-decks*)]
+(defn -main [& {:keys [soft-17?]}]
+  (loop [game (new-game 500 *total-decks* soft-17?)]
     (if (= :quit game)
       "Goodbye!"
       (recur (player-turn game)))))
